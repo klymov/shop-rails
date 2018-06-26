dc97e323b26349ec410106c61e2b6d08620b7c70
Ihor Klymov <gtp.klimov@gmail.com>
2018-06-26 13:42:15 +0300

    first commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,74 @@
+class ItemsController < ApplicationController
+
+  before_action :find_item, only: [:new, :create, :show, :edit, :update, :destroy]
+
+  def index
+    @items = Item.all
+  end
+
+  # показ дорогих товаров
+  def expensive
+    @items = Item.where('price > 100')
+  end
+
+  # /items/1 GET
+  def show
+    unless @item = Item.where(id: params[:id]).first
+      render text: 'Page not found',status: 404
+    end
+  end
+
+  # new -> create
+  # /items/new GET
+  def new
+    @item = Item.new
+  end
+
+  # /items POST
+  def create
+    @item = Item.create(item_params)
+    if @item.errors.empty?
+      redirect_to item_path(@item)
+    else
+      render 'new'
+    end
+  end
+
+  # edit -> update
+  # /items/1/edit  GET
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  # /items/1  PUT
+  def update
+    item_params
+    @item.updated_attributes(item_params)
+    if @item.errors.empty?
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
+  # /items/1  DELETE
+  def destroy
+    @item.destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :quantity, :category)
+  end
+
+  def find_item
+    @item = Item.where(id: params[:id]).first
+    render_404 unless @item
+  end
+
+  def render_404
+    render file: '/public/404.html', status: 404
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+
+  validates :name, :description, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :price, numericality: { greater_than: 0, allow_nil: true }
+  validates :quantity, numericality: { greater_than: 0, allow_nil: true }
+
+
+=begin
+  belongs_to  :category
+  has_many    :positions
+  has_many    :carts, through: :positions
+=end
+
 end
