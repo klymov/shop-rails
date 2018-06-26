125a3429e84ad5fb9f6a4a70e7125c402f4bbe76
Ihor Klymov <gtp.klimov@gmail.com>
2018-06-26 15:12:24 +0300

    css index bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'items' => 'items'
+
+  get 'items/new'
+  get 'items/create' => 'items#create'
+
   root 'items#index'
 
 end
